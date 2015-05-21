e98c79e2c84e4be37b9eb8f14318f0beb5280b93
Shin'ya Ueoka <ibenza@i-beam.org>
2015-05-21 22:05:09 +0900

    Add ruby files

---
+++ ruby/math.rb
@@ -0,0 +1,7 @@
+def negative(x)
+  -x
+end
+
+def power(x, y)
+  x ** y
+end
---
+++ ruby/test.rb
@@ -0,0 +1,14 @@
+require './math'
+require 'test/unit'
+
+class MathTest < Test::Unit::TestCase
+  def test_negative
+    assert_equal(negative(5), -5)
+    assert_equal(negative(0), 0)
+  end
+
+  def test_power
+    assert_equal(power(2, 3), 8)
+    assert_equal(power(5, 0), 1)
+  end
+end
