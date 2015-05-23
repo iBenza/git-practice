ca34c740cc79b52cbe522c450b7d57b3c890e877
Furutsuki <theoldmoon0602@gmail.com>
2015-05-23 16:14:53 +0900

    added new functions: absolute(x), is_even(x), is_odd(x)

--- python/math.py
+++ python/math.py
@@ -3,3 +3,12 @@ def negative(x):
 
 def power(x, y):
     return x ** y
+
+def absolute(x):
+    return x > 0
+
+def is_odd(x):
+    return x % 2 == 0
+
+def is_even(x):
+    return is_odd(x-1)
--- python/test.py
+++ python/test.py
@@ -10,5 +10,24 @@ class MathTest(unittest.TestCase):
         self.assertEqual(math.power(2, 3), 8)
         self.assertEqual(math.power(5, 0), 1)
 
+    def test_absolute(self):
+        self.assertEqual(math.absolute(0), 0)
+        self.assertEqual(math.absolute(10), 10)
+        self.assertEqual(math.absolute(-10), -10)
+
+    def test_is_even(self):
+        self.assertEqual(math.is_even(0), True)
+        self.assertEqual(math.is_even(5), False)
+        self.assertEqual(math.is_even(-5), False)
+        self.assertEqual(math.is_even(-10), True)
+        self.assertEqual(math.is_even(10), True)
+
+    def test_is_odd(self):
+        self.assertEqual(math.is_odd(0), False)
+        self.assertEqual(math.is_odd(5), True)
+        self.assertEqual(math.is_odd(-5), True)
+        self.assertEqual(math.is_odd(-10), False)
+        self.assertEqual(math.is_odd(10), False)
+
 if __name__ == "__main__":
     unittest.main()
